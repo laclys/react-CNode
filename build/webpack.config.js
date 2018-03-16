dbb00a132e5c530156ebd5b642448e639e41e959
Lac <laclys@126.com>
2018-03-17 00:51:15 +0800

    webpack配置react解析配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -10,5 +10,14 @@ module.exports ={
     filename: '[name].[hash].js',  //name代表entry对应的名字; hash代表 整个app打包完成后根据内容加上hash。一旦整个文件内容变更，hash就会变化
     path: path.join(__dirname, '../dist'), // 打包好之后的输出路径
     publicPath: '/public' // 静态资源文件引用时的路径（加在引用静态资源前面的）
+  },
+  // 配置loader
+  module: {
+    rules: [
+      {
+        test: /.jsx$/, //使用loader的目标文件。这里是.jsx
+        loader: 'babel-loader'
+      }
+    ]
   }
 }
