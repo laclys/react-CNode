1b9997ca82cae7ce41c5eeca260b4a793ff60ecb
Lac <laclys@126.com>
2018-03-18 14:17:59 +0800

    add note

--- server/util/dev-static.js
+++ server/util/dev-static.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const webpack = require('webpack')
 const path = require('path')
-const MemoryFs = require('memory-fs')  // 从内存中读取信息
+const MemoryFs = require('memory-fs')  // 从内存中读取信息 和fs API一模一样
 const ReactDomServer = require('react-dom/server')
 const proxy = require('http-proxy-middleware')
 
@@ -19,11 +19,12 @@ const getTemplate = () => {
 
 const Module = module.constructor
 const mfs = new MemoryFs()
-const serverCompiler = webpack(serverConfig)
-serverCompiler.outputFileSystem = mfs // 通过memory-fs进行读写 (内存读写比硬盘读写快很多)
+const serverCompiler = webpack(serverConfig)  // webpack在node中调用
+serverCompiler.outputFileSystem = mfs // 通过memory-fs进行读写，加快他的打包速度 (内存读写比硬盘读写快很多)
 
 let serverBundle
 
+// 目的是拿到打包下来的内容
 serverCompiler.watch({}, (err, stats) => {
   if (err) throw err
   stats= stats.toJson()
@@ -34,7 +35,8 @@ serverCompiler.watch({}, (err, stats) => {
     serverConfig.output.path,
     serverConfig.output.filename
   )
-  const bundle = mfs.readFileSync(bundlePath, 'utf-8')
+  const bundle = mfs.readFileSync(bundlePath, 'utf-8')  // 编译出来是一串字符串
+  // hack：通过new一个Moudle，通过——compile转化字符串，成为nodeJs可以使用的moudle
   const m = new Module()
   m._compile(bundle, 'server-entry.js') //动态编译需要指定一个文件名。不然他无法在缓存中存储这部分内容
   serverBundle = m.exports.default
