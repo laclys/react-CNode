465ab7dd82f50704ef8e80a8c36119f5b538205b
Lac <laclys@126.com>
2018-03-18 12:28:29 +0800

    server区分开发/生产环境

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 const express =require('express')
 const fs = require('fs')
 const ReactSSR = require('react-dom/server')
-const serverEntry = require('../dist/server-entry').default
 const path = require('path')
 
-const tplPath = path.join(__dirname, '../dist/index.html')
-
-const template = fs.readFileSync(tplPath, 'utf8') // 不指定utf-8,默认是buffer
+const isDev = process.env.NODE_ENV === 'development'
 
 const app = express()
 
-app.use('/public', express.static(path.join(__dirname, '../dist'))) //静态文件指定请求返回
-
-app.get('*', (req, res) => {
-  const appStr = ReactSSR.renderToString(serverEntry)
-  res.send(template.replace('<!--app-->', appStr))
-})
+if (!isDev) {
+  const serverEntry = require('../dist/server-entry').default
+  const tplPath = path.join(__dirname, '../dist/index.html')
+  const template = fs.readFileSync(tplPath, 'utf8') // 不指定utf-8,默认是buffer
+  app.use('/public', express.static(path.join(__dirname, '../dist'))) //静态文件指定请求返回
+  app.get('*', (req, res) => {
+    const appStr = ReactSSR.renderToString(serverEntry)
+    res.send(template.replace('<!--app-->', appStr))
+  })
+} esle {
+  
+}
 
 app.listen(2333, () => {
   console.log('server is listening on 2333')
