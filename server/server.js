3bc29bc42e6f5193a2b940a1b6cb99c00c6de603
Lac <laclys@126.com>
2018-03-24 13:46:35 +0800

    fix server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const favicon = require('serve-favicon')
 const fs = require('fs')
-const ReactSSR = require('react-dom/server')
+const serverRender = require('./util/server-render')
 const path = require('path')
 const bodyParser = require('body-parser')
 const session = require('express-session')
@@ -28,18 +28,24 @@ app.use('/api/user', require('./util/handle-login'))
 app.use('/api', require('./util/proxy'))
 
 if (!isDev) {
-  const serverEntry = require('../dist/server-entry').default
-  const tplPath = path.join(__dirname, '../dist/index.html')
+  const serverEntry = require('../dist/server-entry')
+  const tplPath = path.join(__dirname, '../dist/server.ejs')
   const template = fs.readFileSync(tplPath, 'utf8') // 不指定utf-8,默认是buffer
   app.use('/public', express.static(path.join(__dirname, '../dist'))) //静态文件指定请求返回
-  app.get('*', (req, res) => {
-    const appStr = ReactSSR.renderToString(serverEntry)
-    res.send(template.replace('<!--app-->', appStr))
+  app.get('*', (req, res, next) => {
+    serverRender(serverEntry, template, req, res)
+      .catch(next)
   })
 } else {
   const devStatic = require('./util/dev-static')
   devStatic(app)
 }
+
+// error handle
+app.unsubscribe(function(error, req, res, next) {
+  console.log(error)
+  res.status(500).send(error)
+})
 
 app.listen(2333, () => {
   console.log('server is listening on 2333')
